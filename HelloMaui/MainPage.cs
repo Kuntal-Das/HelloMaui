717e4ba156a46e10eaa9799bb0d71b85b0c72f29
Kuntal Das <42152227+Kuntal-Das@users.noreply.github.com>
2024-12-27 19:56:50 +0530

    margin and paddings

--- HelloMaui/MainPage.cs
+++ HelloMaui/MainPage.cs
@@ -36,67 +36,72 @@ public class MainPage : BaseContentPage
         Content = new ScrollView()
         {
             Content = new Grid()
-            {
-                RowSpacing = 12,
-                ColumnSpacing = 12,
-                BackgroundColor = Colors.LightSteelBlue,
-                RowDefinitions = GridRowsColumns.Rows.Define(
-                    (Row.Image, GridLength.Star),
-                    (Row.Label, 32),
-                    (Row.Entry, 40),
-                    (Row.LargeTextLabel, GridLength.Star)
-                ),
-                ColumnDefinitions = GridRowsColumns.Columns.Define(
-                    (Column.Entry1, GridLength.Star),
-                    (Column.Entry2, GridLength.Star),
-                    (Column.Entry3, GridLength.Star)
-                ),
-
-                Children =
                 {
-                    new Image()
-                        // .Size(500, 250)
-                        // .Aspect(Aspect.AspectFill)
-                        .Source("dotnet_bot")
-                        .Row(Row.Image)
-                        .ColumnSpan(3),
+                    RowSpacing = 12,
+                    ColumnSpacing = 12,
+                    BackgroundColor = Colors.LightSteelBlue,
+                    RowDefinitions = GridRowsColumns.Rows.Define(
+                        (Row.Image, GridLength.Star),
+                        (Row.Label, GridLength.Auto),
+                        (Row.Entry, 40),
+                        (Row.LargeTextLabel, GridLength.Star)
+                    ),
+                    ColumnDefinitions = GridRowsColumns.Columns.Define(
+                        (Column.Entry1, GridLength.Star),
+                        (Column.Entry2, GridLength.Star),
+                        (Column.Entry3, GridLength.Star)
+                    ),
 
-                    new Label()
-                        .Text("Hello MAUI!")
-                        .TextColor(Colors.Black)
-                        .Center()
-                        .TextCenter()
-                        .Row(Row.Label)
-                        .ColumnSpan(3),
+                    Children =
+                    {
+                        new Image()
+                            // .Size(500, 250)
+                            // .Aspect(Aspect.AspectFill)
+                            .Source("dotnet_bot")
+                            .Row(Row.Image)
+                            .ColumnSpan(3),
 
-                    new Entry()
-                        .Placeholder("First Entry", Colors.DarkOliveGreen)
-                        .TextColor(Colors.Black)
-                        .Row(Row.Entry)
-                        .Column(Column.Entry1),
+                        new Label()
+                            .BackgroundColor(Colors.Green)
+                            .Paddings(5, 10, 15, 30)
+                            .Margins(30, 15, 10, 5)
+                            .Text("Hello MAUI!")
+                            .TextColor(Colors.Black)
+                            .Center()
+                            .TextCenter()
+                            .Row(Row.Label)
+                            .ColumnSpan(3),
 
-                    new Entry()
-                        .Placeholder("Second Entry", Colors.DarkOliveGreen)
-                        .TextColor(Colors.Black)
-                        .Row(Row.Entry)
-                        .Column(Column.Entry2),
+                        new Entry()
+                            .Placeholder("First Entry", Colors.DarkOliveGreen)
+                            .TextColor(Colors.Black)
+                            .Row(Row.Entry)
+                            .Column(Column.Entry1),
 
-                    new Entry()
-                        .Placeholder("Third Entry", Colors.DarkOliveGreen)
-                        .TextColor(Colors.Black)
-                        .Row(Row.Entry)
-                        .Column(Column.Entry3),
+                        new Entry()
+                            .Placeholder("Second Entry", Colors.DarkOliveGreen)
+                            .TextColor(Colors.Black)
+                            .Row(Row.Entry)
+                            .Column(Column.Entry2),
 
-                    new Label()
-                        {
-                            LineBreakMode = LineBreakMode.WordWrap,
-                        }
-                        .Text(longText)
-                        .TextCenter()
-                        .Row(Row.LargeTextLabel)
-                        .ColumnSpan(All<Column>()),
-                }
-            }.Top()
+                        new Entry()
+                            .Placeholder("Third Entry", Colors.DarkOliveGreen)
+                            .TextColor(Colors.Black)
+                            .Row(Row.Entry)
+                            .Column(Column.Entry3),
+
+                        new Label()
+                            {
+                                LineBreakMode = LineBreakMode.WordWrap,
+                            }
+                            .Text(longText)
+                            .TextCenter()
+                            .Row(Row.LargeTextLabel)
+                            .ColumnSpan(All<Column>()),
+                    }
+                }.Top().CenterHorizontal()
+                .Padding(12)
+                .Margins(0, 6, 0, 0)
         };
     }
 }
