28fc7623f044ed19c270fc0424084e6763e65cb9
Kuntal Das <42152227+Kuntal-Das@users.noreply.github.com>
2024-12-27 19:23:59 +0530

    Grid Layout

--- HelloMaui/MainPage.cs
+++ HelloMaui/MainPage.cs
@@ -3,60 +3,85 @@ using Microsoft.Maui.Layouts;
 
 namespace HelloMaui;
 
+internal enum Row
+{
+    Image,
+    Label,
+    Entry,
+    Empty
+}
+
+internal enum Column
+{
+    Entry1,
+    Entry2,
+    Entry3
+}
+
 public class MainPage : BaseContentPage
 {
-    private const int imageWidthRequest = 500;
-    private const int imageHeightRequest = 250;
-    private const int labelHeightRequest = 32;
-    private const int entryHeightRequest = 48;
-    private const int absoluteLayoutChildrenSpacing = 12;
+    // private const int imageWidthRequest = 500;
+    // private const int imageHeightRequest = 250;
+    // private const int labelHeightRequest = 32;
+    // private const int entryHeightRequest = 48;
+    // private const int absoluteLayoutChildrenSpacing = 12;
 
     public MainPage()
     {
         BackgroundColor = Colors.DarkViolet;
         Title = "Hello Maui";
-        Content = new AbsoluteLayout()
+        Content = new Grid()
         {
-            // Spacing = 12,
+            RowSpacing = 12,
+            ColumnSpacing = 12,
             BackgroundColor = Colors.LightSteelBlue,
+            RowDefinitions = GridRowsColumns.Rows.Define(
+                (Row.Image, GridLength.Star),
+                (Row.Label, 32),
+                (Row.Entry, 40)
+                // (Row.Empty, GridLength.Star)
+            ),
+            ColumnDefinitions = GridRowsColumns.Columns.Define(
+                (Column.Entry1, GridLength.Star),
+                (Column.Entry2, GridLength.Star),
+                (Column.Entry3, GridLength.Star)
+            ),
+
             Children =
             {
                 new Image()
-                    .Size(imageWidthRequest, imageHeightRequest)
-                    .Aspect(Aspect.AspectFill)
+                    // .Size(500, 250)
+                    // .Aspect(Aspect.AspectFill)
                     .Source("dotnet_bot")
-                    .LayoutFlags(AbsoluteLayoutFlags.PositionProportional)
-                    .LayoutBounds(0.5, 0),
+                    .Row(Row.Image)
+                    .ColumnSpan(3),
+
                 new Label()
-                    .Height(labelHeightRequest)
                     .Text("Hello MAUI!")
                     .TextColor(Colors.Black)
                     .Center()
                     .TextCenter()
-                    .LayoutFlags(AbsoluteLayoutFlags.XProportional)
-                    .LayoutBounds(0.5, imageHeightRequest + absoluteLayoutChildrenSpacing),
+                    .Row(Row.Label)
+                    .ColumnSpan(3),
 
                 new Entry()
-                    .Placeholder("First Entry", Colors.DarkGray)
+                    .Placeholder("First Entry", Colors.DarkOliveGreen)
                     .TextColor(Colors.Black)
-                    .LayoutFlags(AbsoluteLayoutFlags.XProportional | AbsoluteLayoutFlags.WidthProportional)
-                    .LayoutBounds(0, imageHeightRequest + labelHeightRequest + (absoluteLayoutChildrenSpacing * 2), 0.3,
-                        entryHeightRequest),
+                    .Row(Row.Entry)
+                    .Column(Column.Entry1),
 
                 new Entry()
-                    .Placeholder("Second Entry", Colors.DarkGray)
+                    .Placeholder("Second Entry", Colors.DarkOliveGreen)
                     .TextColor(Colors.Black)
-                    .LayoutFlags(AbsoluteLayoutFlags.XProportional | AbsoluteLayoutFlags.WidthProportional)
-                    .LayoutBounds(0.5, imageHeightRequest + labelHeightRequest + (absoluteLayoutChildrenSpacing * 2), 0.3,
-                        entryHeightRequest),
+                    .Row(Row.Entry)
+                    .Column(Column.Entry2),
 
                 new Entry()
-                    .Placeholder("Third Entry", Colors.DarkGray)
+                    .Placeholder("Third Entry", Colors.DarkOliveGreen)
                     .TextColor(Colors.Black)
-                    .LayoutFlags(AbsoluteLayoutFlags.XProportional | AbsoluteLayoutFlags.WidthProportional)
-                    .LayoutBounds(1, imageHeightRequest + labelHeightRequest + (absoluteLayoutChildrenSpacing * 2), 0.3,
-                        entryHeightRequest),
+                    .Row(Row.Entry)
+                    .Column(Column.Entry3)
             }
-        };
+        }.Top();
     }
 }
