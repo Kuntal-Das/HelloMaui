bd85304a4eccb921ac92643b6602e9f6701fcec8
Kuntal Das <42152227+Kuntal-Das@users.noreply.github.com>
2024-12-27 12:34:53 +0530

    absolute layout

--- HelloMaui/MainPage.cs
+++ HelloMaui/MainPage.cs
@@ -1,47 +1,61 @@
 using CommunityToolkit.Maui.Markup;
+using Microsoft.Maui.Layouts;
 
 namespace HelloMaui;
 
 public class MainPage : BaseContentPage
 {
+    private const int imageWidthRequest = 500;
+    private const int imageHeightRequest = 250;
+    private const int labelHeightRequest = 32;
+    private const int entryHeightRequest = 48;
+    private const int absoluteLayoutChildrenSpacing = 12;
+
     public MainPage()
     {
         BackgroundColor = Colors.DarkViolet;
         Title = "Hello Maui";
-        Content = new VerticalStackLayout()
+        Content = new AbsoluteLayout()
         {
+            // Spacing = 12,
             BackgroundColor = Colors.LightSteelBlue,
-            Spacing = 12,
             Children =
             {
                 new Image()
-                    .Size(500, 250)
+                    .Size(imageWidthRequest, imageHeightRequest)
                     .Aspect(Aspect.AspectFill)
-                    .Source("dotnet_bot"),
+                    .Source("dotnet_bot")
+                    .LayoutFlags(AbsoluteLayoutFlags.PositionProportional)
+                    .LayoutBounds(0.5, 0),
                 new Label()
+                    .Height(labelHeightRequest)
                     .Text("Hello MAUI!")
                     .TextColor(Colors.Black)
                     .Center()
-                    .TextCenter(),
-                new HorizontalStackLayout()
-                {
-                    BackgroundColor = Colors.ForestGreen,
-                    Spacing = 12,
-                    Children =
-                    {
-                        new Entry()
-                            .Placeholder("First Entry", Colors.DarkGray)
-                            .TextColor(Colors.Black),
-                        
-                        new Entry()
-                            .Placeholder("Second Entry", Colors.DarkGray)
-                            .TextColor(Colors.Black),
-                        
-                        new Entry()
-                            .Placeholder("Third Entry", Colors.DarkGray)
-                            .TextColor(Colors.Black),
-                    },
-                }.Center(),
+                    .TextCenter()
+                    .LayoutFlags(AbsoluteLayoutFlags.XProportional)
+                    .LayoutBounds(0.5, imageHeightRequest + absoluteLayoutChildrenSpacing),
+
+                new Entry()
+                    .Placeholder("First Entry", Colors.DarkGray)
+                    .TextColor(Colors.Black)
+                    .LayoutFlags(AbsoluteLayoutFlags.XProportional | AbsoluteLayoutFlags.WidthProportional)
+                    .LayoutBounds(0, imageHeightRequest + labelHeightRequest + (absoluteLayoutChildrenSpacing * 2), 0.3,
+                        entryHeightRequest),
+
+                new Entry()
+                    .Placeholder("Second Entry", Colors.DarkGray)
+                    .TextColor(Colors.Black)
+                    .LayoutFlags(AbsoluteLayoutFlags.XProportional | AbsoluteLayoutFlags.WidthProportional)
+                    .LayoutBounds(0.5, imageHeightRequest + labelHeightRequest + (absoluteLayoutChildrenSpacing * 2), 0.3,
+                        entryHeightRequest),
+
+                new Entry()
+                    .Placeholder("Third Entry", Colors.DarkGray)
+                    .TextColor(Colors.Black)
+                    .LayoutFlags(AbsoluteLayoutFlags.XProportional | AbsoluteLayoutFlags.WidthProportional)
+                    .LayoutBounds(1, imageHeightRequest + labelHeightRequest + (absoluteLayoutChildrenSpacing * 2), 0.3,
+                        entryHeightRequest),
             }
         };
     }
